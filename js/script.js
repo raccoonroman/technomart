08e1d71cc742e83d5a698a1c803f61d0cfa9c47b
Roman Kushnir <kromanjs@gmail.com>
2018-06-05 23:41:15 +0300

    added modal windows

--- js/script.js
+++ js/script.js
@@ -56,14 +56,14 @@ var emailField = popup.querySelector("[name=feedback-email-field]");
 
 btnFeedback.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.classList.add("modal-feedback-show");
+	popup.classList.add("modal-show");
 	overlay.classList.add("modal-overlay-show");
 	nameField.focus();
 });
 
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.classList.remove("modal-feedback-show");
+	popup.classList.remove("modal-show");
 	overlay.classList.remove("modal-overlay-show");
 	popup.classList.remove("modal-feedback-error");
 });
@@ -80,10 +80,36 @@ form.addEventListener("submit", function (evt) {
 window.addEventListener("keydown", function (evt) {
 	if (evt.keyCode === 27) {
 		evt.preventDefault();
-		if (popup.classList.contains("modal-feedback-show")) {
-			popup.classList.remove("modal-feedback-show");
+		if (popup.classList.contains("modal-show")) {
+			popup.classList.remove("modal-show");
 			overlay.classList.remove("modal-overlay-show");
 			popup.classList.remove("modal-feedback-error");
+		}
+	}
+});
+
+var mapLink = document.querySelector(".map-small");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+    overlay.classList.add("modal-overlay-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	mapPopup.classList.remove("modal-show");
+	overlay.classList.remove("modal-overlay-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		if (mapPopup.classList.contains("modal-show")) {
+			mapPopup.classList.remove("modal-show");
+			overlay.classList.remove("modal-overlay-show");
 		}
 	}
 });
